cd514bf3da59052b8892c1a5bb8557332d6f6f57
Melissa Guachun <1212malka@gmail.com>
2022-05-13 14:36:24 -0400

    instructions

---
+++ src/HowTo.js
@@ -0,0 +1,14 @@
+import React from "react";
+const HowTo = () => {
+  
+  
+    return (
+      <div className="how-to">
+
+      <h3>To get started, click a title to unlock Ghibli stats! </h3>
+  
+      </div>
+    );
+  }
+  
+  export default HowTo;
