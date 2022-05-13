0e4caae33532619e64074a0d79015d08a6e9de17
Melissa Guachun <1212malka@gmail.com>
2022-05-13 14:36:16 -0400

    setter getter, imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import MovieList from './MovieList';
+import MovieInfo from './MovieInfo';
 
-function App() {
+
+
+const App = () => {
+  const [film, setFilms] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Studio Ghibli Collection</h1>
+
+      <MovieInfo film={film} />
+      <MovieList setter={setFilms} />
+      
+      
+
     </div>
   );
 }
